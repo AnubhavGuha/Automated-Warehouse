9253fc5b617532a55421d60c725f596559e5a1f0
danpilgrim <d.daniel.pilgrim@gmail.com>
2019-12-04 17:21:14 -0700

    arranged code better

--- test_all_actions.sh
+++ test_all_actions.sh
@@ -3,22 +3,22 @@
 printf "\n"
 printf "     ***** RUNNING TEST (MOVE POS X) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/move_posx.asp -c m=20 -t4
+clingo warehouse.lp actionTests/move_posx.asp output/debug.lp -c m=20 -t4
 
 printf "\n"
 printf "     ***** RUNNING TEST (MOVE NEG X) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/move_negx.asp -c m=20 -t4
+clingo warehouse.lp actionTests/move_negx.asp output/debug.lp -c m=20 -t4
 
 printf "\n"
 printf "     ***** RUNNING TEST (MOVE POS Y) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/move_posy.asp -c m=20 -t4
+clingo warehouse.lp actionTests/move_posy.asp output/debug.lp -c m=20 -t4
 
 printf "\n"
 printf "     ***** RUNNING TEST (MOVE NEG Y) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/move_negy.asp -c m=20 -t4
+clingo warehouse.lp actionTests/move_negy.asp output/debug.lp -c m=20 -t4
 
 
 ### Test Pickup
@@ -26,7 +26,7 @@ clingo warehouse.lp actionTests/move_negy.asp -c m=20 -t4
 printf "\n"
 printf "     ***** RUNNING TEST (PICKUP) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/pickup.asp -c m=20 -t4
+clingo warehouse.lp actionTests/pickup.asp output/debug.lp -c m=20 -t4
 
 
 ### Test Putdown
@@ -34,7 +34,7 @@ clingo warehouse.lp actionTests/pickup.asp -c m=20 -t4
 printf "\n"
 printf "     ***** RUNNING TEST (PUTDOWN) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/putdown.asp -c m=20 -t4
+clingo warehouse.lp actionTests/putdown.asp output/debug.lp -c m=20 -t4
 
 
 ### Test Deliver
@@ -42,4 +42,4 @@ clingo warehouse.lp actionTests/putdown.asp -c m=20 -t4
 printf "\n"
 printf "     ***** RUNNING TEST (DELIVER) *****\n"
 printf "\n"
-clingo warehouse.lp actionTests/deliver.asp -c m=20 -t4
+clingo warehouse.lp actionTests/deliver.asp output/debug.lp -c m=20 -t4
